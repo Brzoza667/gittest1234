08c42c12bbb90ef67339a930b652dd0a152aa782
Your NameBrzoza <brzoskatomasz@gmail.com>
2022-02-19 14:44:00 +0000

    update zadan

---
+++ src/pl/sdacademy/java/basic/exercises/task7/Task7.java
@@ -0,0 +1,5 @@
+package pl.sdacademy.java.basic.exercises.task7;
+
+public class Task7 {
+//    Praca domowa!!!!!!!!!!!!!!!!!!!
+}
---
+++ src/pl/sdacademy/java/basic/exercises/task9/Task9.java
@@ -0,0 +1,5 @@
+package pl.sdacademy.java.basic.exercises.task9;
+
+public class Task9 {
+    
+}
