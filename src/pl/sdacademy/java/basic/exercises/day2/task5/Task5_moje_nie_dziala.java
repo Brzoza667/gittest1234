8c9b8691bf0e18478573bb61c2cd332dcb7360bd
Your NameBrzoza <brzoskatomasz@gmail.com>
2022-02-20 11:35:25 +0000

    up to 6

---
+++ src/pl/sdacademy/java/basic/exercises/day2/task5/Task5_moje_nie_dziala.java
@@ -0,0 +1,18 @@
+//package pl.sdacademy.java.basic.exercises.day2.task5;
+//
+//public class Task5_moje_nie_dziala {
+//    public static void main(String[] args) {
+//        String input = "Ala ma kota";
+//        String character = (char) searchedCharacter = 'a';
+//
+//        System.out.println("input" + input);
+//        System.out.println("searched charcter" + searchedCharacter);
+//        System.out.println("result" + result(input));
+//
+//
+//    }
+//
+//    private static String result(String input) {
+//            return input.chars();
+//    }
+//}
